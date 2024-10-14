ab890262ca7079c65c4c1985e3a557d1ed4a588a
Hunter <113154751+HunterWinger@users.noreply.github.com>
2024-10-14 03:18:11 -0700

    Finishing touch before demo video

--- HomeworkOneProject/obj/Debug/net8.0/HomeworkOneProject.AssemblyInfo.cs
+++ HomeworkOneProject/obj/Debug/net8.0/HomeworkOneProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeworkOneProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2131aa39899c69d9a480220842ae60cb793bf18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39c4038fbe6d86f134b70f596e5af63377a6fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeworkOneProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeworkOneProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HomeworkOneTest/obj/Debug/net8.0/HomeworkOneTest.AssemblyInfo.cs
+++ HomeworkOneTest/obj/Debug/net8.0/HomeworkOneTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeworkOneTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c39c4038fbe6d86f134b70f596e5af63377a6fe4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeworkOneTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeworkOneTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
